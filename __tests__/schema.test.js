44b5644567097d686fbb08ec1605d0cb7b21b013
Dave Trost <davetrost@gmail.com>
2019-09-18 17:51:53 -0700

    schema validate function passing jest

---
+++ __tests__/schema.test.js
@@ -0,0 +1,82 @@
+const Schema = require('../lib/Schema');
+const ModelError = require('../lib/Errors');
+
+describe('schema module performs validation for incoming data', () => {
+
+  const personSchema = new Schema({
+    firstName: 'string',
+    lastName: 'string',
+    // hair: 'object',
+    //favoriteFoods: ['string'],
+    married: 'boolean',
+    kids: 'number',
+  });
+
+  it('passes good model data', () => {
+    const personModel = {
+      firstName: 'Chris',
+      lastName: 'Sample',
+      // hair: {
+      //   type: 'wavy',
+      //   color: 'brown',
+      // },
+      // favoriteFoods: [
+      //   'pizza',
+      //   'cupcakes',
+      //   'salmon',
+      // ],
+      married: true,
+      kids: 3,
+    };
+    const person = personSchema.validate(personModel);
+    expect(person).toEqual(personModel);
+  });
+  
+  it('throws an error when bad model data is encountered', () => {
+    const personModel = {
+      firstName: true,
+      lastName: 'Sample',
+    };
+    const errorToBeThrown = () => personSchema.validate(personModel);
+    expect(errorToBeThrown).toThrow('this model is not compliant with the schema');
+  });
+
+  it('ignores extra values in the incoming data', () => {
+    const personModel = {
+      firstName: 'Chris',
+      lastName: 'Sample',
+      // hair: {
+      //   type: 'wavy',
+      //   color: 'brown',
+      // },
+      // favoriteFoods: [
+      //   'pizza',
+      //   'cupcakes',
+      //   'salmon',
+      // ],
+      married: true,
+      kids: 3,
+    };
+    const personModelWithOtherStuff = {
+      firstName: 'Chris',
+      lastName: 'Sample',
+      hair: {
+        type: 'wavy',
+        color: 'brown',
+      },
+      favoriteFoods: [
+        'pizza',
+        'cupcakes',
+        'salmon',
+      ],
+      married: true,
+      kids: 3,
+      grandkids: 5,
+      nieces: 9,
+    };
+
+    const person = personSchema.validate(personModelWithOtherStuff);
+    expect(person).toEqual(personModel);
+  });
+
+});
