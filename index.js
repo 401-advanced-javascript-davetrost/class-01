5c368a9d282cc396569705dca4187f19f4886232
Dave Trost <davetrost@gmail.com>
2019-09-19 13:45:23 -0700

    save(), get(), getAll(), alive

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const DocumentCollection = require('./lib/document-collection');
 
-const documents = new DocumentCollection(/* path to use */);
+const documents = new DocumentCollection('./document-collection');
 
-// write some code to exercise your document collection
+const jsonObject = { 'name':'John', 'age':30, 'car':null };
+documents.save(jsonObject)
+  .then(object => {
+    console.log('save( object ) =', object);
+    return object;
+  })
+  .then(object => {
+    return documents.get(object.id)
+      .then(content => {
+        console.log('get(', object.id, ') = ', content);
+        return content;
+      })
+      .catch(err => console.log(err));
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
+documents.getAll()
+  .then(content => {
+    console.log('readdir() =', content);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
 
