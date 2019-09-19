66321dffc308a462d1cbee6a106e195a20e5b1f2
Dave Trost <davetrost@gmail.com>
2019-09-19 09:18:08 -0700

    starter code from lab setup

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-'use strict';
+const DocumentCollection = require('./lib/document-collection');
 
-/**
- * Simple Server
- * @module index
- */
+const documents = new DocumentCollection(/* path to use */);
 
-require('dotenv').config();
-const validator = require('./lib/validator.js');
+// write some code to exercise your document collection
 
-const express = require('express');
-const app = express();
-app.use('/docs', express.static('./docs/'));
-
-/**
- * 
- * / Request Handler (all routes)
- * @param req
- * @param res
- */
-
-app.get('', requestHandler);
-
-function requestHandler(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.statusCode = 200;
-  res.write('is "false" a boolean? ' + validator.isBoolean('false') + '\n');  // false
-  res.write('is true a boolean? ' + validator.isBoolean(true) + '\n');  // true
-  res.write('is "true" a boolean? ' + validator.isBoolean('true') + '\n');  // false
-  res.write('\n');  // false
-  res.write('Please browse to /docs to view documentation for this project.\n');  // false
-  res.end();
-}
-
-app.listen(process.env.PORT, () => console.log('node.js server running on port', process.env.PORT));
